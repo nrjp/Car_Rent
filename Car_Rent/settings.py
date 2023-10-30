712672a65a53c90e69585b0048b33674c15d85b9
Niraj Patharkar <nirajpathrkar@gmail.com>
2023-10-30 19:09:41 +0530

    Update settings.py
    allowed hosts

--- Car_Rent/settings.py
+++ Car_Rent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*aewfx9&2@iyp*g$p)r)lx_72+)06dxnd5k!=lhdi=f^adv0=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.83.242.18']
 
 
 # Application definition
